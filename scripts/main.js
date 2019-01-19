e32d5e892be2ce79b69cab94386479e194f1ffea
Roberto S. Ramos Jr <robertosrjr@gmail.com>
2019-01-18 22:58:01 -0200

    commit inicial

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// Log the user in and out on click
+const popupUri = 'popup.html';
+$('#login  button').click(() => solid.auth.popupLogin({ popupUri }));
+$('#logout button').click(() => solid.auth.logout());
+
+// Update components to match the user's login status
+solid.auth.trackSession(session => {
+  const loggedIn = !!session;
+  $('#login').toggle(!loggedIn);
+  $('#logout').toggle(loggedIn);
+  $('#user').text(session && session.webId);
+});
